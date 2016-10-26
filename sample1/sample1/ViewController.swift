692f57f78bc198a9b56c456a97534b609d203216
gembaf <gembaf.github@gmail.com>
2016-10-26 17:04:09 +0900

    足し算のみ可能な電卓を実装

--- sample1/sample1/ViewController.swift
+++ sample1/sample1/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lhsTextField: UITextField!
+    @IBOutlet weak var rhsTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calculateButton(_ sender: AnyObject) {
+        let lhs = Int(lhsTextField.text!)
+        let rhs = Int(rhsTextField.text!)
+        resultLabel.text = String(lhs! + rhs!)
+    }
 }
 
