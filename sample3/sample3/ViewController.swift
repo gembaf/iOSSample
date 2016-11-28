aca89ae27fc4908b2d50b21f29a600a4c1d62743
gembaf <gembaf.github@gmail.com>
2016-11-28 16:41:58 +0900

    Realmを用いたサンプルプログラムを作成

--- sample3/sample3/ViewController.swift
+++ sample3/sample3/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let path = paths[0] + "/test.realm"
+        let url = NSURL(fileURLWithPath: path) as URL
+        let realm = try! Realm(fileURL: url)
+
+        let me = User()
+        me.name = "ほげほげ"
+        
+        try! realm.write {
+            realm.add(me)
+        }
+        
+        var text = ""
+        
+        for user in realm.objects(User) {
+            text += user.name + "\n"
+        }
+        
+        label.text = text
     }
 
     override func didReceiveMemoryWarning() {
