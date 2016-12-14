6d569c32ca00c936404d2267de0a0e2d564a56f3
gembaf <gembaf.github@gmail.com>
2016-12-14 16:32:52 +0900

    Websocket通信を用いるアプリの作成

--- sample5/sample5/ViewController.swift
+++ sample5/sample5/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import SocketIO
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
+    var dataList = ""
+    let SocketURL = NSURL(string:"http://localhost:8080/")
+    var socket: SocketIOClient!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        socket = SocketIOClient(socketURL: SocketURL! as URL)
+        socket.on("connect") { data, ack in
+            print("socket connected!!")
+        }
+        socket.on("disconnect") { data, ack in
+            print("socket disconnected!!")
+        }
+        
+        socket.on("publish") { data, emitter in
+            if let message = data as? [String] {
+                self.dataList += message[0] + "\n"
+                self.textView.text = self.dataList
+            }
+        }
+        socket.connect()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clickButton(_ sender: Any) {
+        socket.emit("publish", "button pushed!!")
     }
-
-
 }
 
