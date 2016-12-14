cfb0f38f0dff71029b0aca941c3314e6cd18dd1c
gembaf <gembaf.github@gmail.com>
2016-12-14 15:42:15 +0900

    sample5 first commit

---
+++ sample5/sample5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample5
+//
+//  Created by Fumiya Gemba on 2016/12/14.
+//  Copyright © 2016年 gembaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
